ddb487ed29e98296aebdba8172e4b1885d4fa1ae
Enne <cheyenne03hug@gmail.com>
2026-07-22 14:39:57 -0400

    add comments/documentation to data-structures file

--- js-scripts/data-structures.js
+++ js-scripts/data-structures.js
@@ -1,57 +1,85 @@
+/**
+ * Node class
+ * 
+ * Creates a node for a linked list
+ */
 class Node {
+    //need value of node (id) to create new node
+    //next node defaults to null
     constructor(id) {
         this.id = id;
         this.next = null;
     }
 
+    //update next node
     setNext(node) {
         this.next = node;
     }
 
+    //get the next node
     getNext() {
         return this.next;
     }
 
+    //get id of the node
     getId() {
         return this.id;
     }
 }
 
+/**
+ * Queue class
+ * 
+ * Use the Node class to create a queue
+ */
 class Queue {
+    //creates empty node
     constructor() {
-        this.front = null;
-        this.tail = null;
-        this.size = 0;
+        this.front = null;  //front of queue (first to be pulled out)
+        this.tail = null;   //end of queue (where new nodes are added)
+        this.size = 0;      //number of elements in queue
     }
 
+    //adds new value into the queue
     enqueue(id) {
+        //create new node
         let node = new Node(id);
         
+        //check if queue is empty
         if (this.size == 0) {
+            //add item to front (empty queue)
             this.front  = node;
             this.tail = node;
         } else {
+            //add item to end of queue
             let curTail = this.tail;
             curTail.setNext(node);
             this.tail = node;
         }
 
+        //increase size of queue
         this.size++;
     }
 
+    //remove and returns value at node at beginning of queue
     dequeue() {
+        //check if queue is empty
         if (this.size == 0) {
             return -1;
         }
 
+        //remove node from front
         let curFront = this.front;
         this.front = curFront.getNext();
 
+        //decrease size of queue
         this.size--;
 
+        //return value of front node
         return curFront.getId();
     }
 
+    //check if queue is empty
     isEmpty() {
         if (this.size == 0) {
             return true;
@@ -60,31 +88,44 @@ class Queue {
         }
     }
 
+    //return size of queue
     getSize() {
         return this.size;
     }
 
+    //print the queue to console (debugging)
     print() {
+        //create pointer in queue
         let current = this.front;
-        // console.log("Print Queue");
 
         let queueString = "";
 
+        //get first item in queue
         if (current != null) {
             queueString = queueString + current.getId();
             current = current.getNext();
         }
 
+        //iterate through queue
         while (current != null) {
-            queueString = queueString + "," + current.getId()
-            // console.log(current.getId());
+            //add id to queuestring
+            queueString = queueString + "," + current.getId();
+
+            //move to next item in queue
             current = current.getNext();
         }
+
+        //print queue info in console
         console.log(queueString);
     }
 }
 
+/**
+ * 5 Queues storing card_ids based on the progress of
+ * the card_id
+ */
 class ProbabilityQueue {
+    //create 5 queues and set min/max level to 1
     constructor() {
         this.seen = new Queue();
         this.recognize = new Queue();
@@ -96,7 +137,9 @@ class ProbabilityQueue {
         this.minLevel = 1;
     }
 
+    //update min and max levels based on emptiness of queues
     updateLevels() {
+        //check for 'lowest' non-empty queue to set min level
         if (!this.seen.isEmpty()) {
             this.minLevel = 1;
         } else if (!this.recognize.isEmpty()) {
@@ -109,6 +152,7 @@ class ProbabilityQueue {
             this.minLevel = 5;
         }
 
+        //check for 'highest' non-empty queue to set max level
         if (!this.mastered.isEmpty()) {
             this.maxLevel = 5;
         } else if (!this.proficent.isEmpty()) {
@@ -120,126 +164,113 @@ class ProbabilityQueue {
         } else if (!this.seen.isEmpty()) {
             this.maxLevel = 1;
         }
-
-
     }
 
+    //add id to seen queue and set minlevel to 1
     addSeen(id) {
         this.seen.enqueue(id);
         this.minLevel = 1;
     }
 
+    //add id to recognize queue and update min/max level
     addRecognize(id) {
         this.recognize.enqueue(id);
         this.updateLevels();
-        // if (this.minLevel > 2) {
-        //     this.minLevel = 2;
-        // }
-
-        // if (this.maxLevel == 1) {
-        //     this.maxLevel = 2;
-        // }
     }
 
+    //add id to retained queue and update min/max level
     addRetained(id) {
         this.retained.enqueue(id);
         this.updateLevels();
-        // if (this.minLevel > 3) {
-        //     this.minLevel = 3;
-        // }
-
-        // if (this.maxLevel == 2) {
-        //     this.maxLevel = 3;
-        // }
     }
 
+    //add id to proficent queue and update min/max level
     addProficent(id) {
         this.proficent.enqueue(id);
         this.updateLevels();
-        // if (this.minLevel > 4) {
-        //     this.minLevel = 4;
-        // }
-
-        // if (this.maxLevel == 3) {
-        //     this.maxLevel = 4;
-        // }
     }
 
+    //add id to mastered queue and update min/max level
     addMastered(id) {
         this.mastered.enqueue(id);
         this.updateLevels();
-        // if (this.minLevel > 5) {
-        //     this.minLevel = 5;
-        // }
-
-        // if (this.maxLevel == 4) {
-        //     this.maxLevel = 5;
-        // }
     }
     
+    //get first element from mastered queue and update max/min levels
+    //if mastered is empty, return -1
     getMastered() {
         if (this.mastered.isEmpty()) {
-            this.updateLevels();
             return -1;
         } else {
             return this.mastered.dequeue();
         }
+
+        this.updateLevels();
     }
 
+    //get first element from proficient queue and update max/min levels
+    //if queue is empty, check the mastered queue
     getProficent() {
         if (this.proficent.isEmpty()) {
-            // this.minLevel = 5;
-            this.updateLevels();
             return this.getMastered();
             
         } else {
             return this.proficent.dequeue();
         }
+
+        this.updateLevels();
     }
 
+    //get first element from retained queue and update max/min levels
+    //if queue is empty, check the proficent queue
     getRetained() {
         if (this.retained.isEmpty()) {
-            // this.minLevel = 4;
-            this.updateLevels();
             return this.getProficent();
             
         } else {
             return this.retained.dequeue();
         }
+
+        this.updateLevels();
     }
 
+    //get first element from recognize queue and update max/min levels
+    //if queue is empty, check the retained queue
     getRecognize() {
         if (this.recognize.isEmpty()) {
-            // this.minLevel = 3;
-            this.updateLevels();
             return this.getRetained();
             
         } else {
             return this.recognize.dequeue();
         }
+
+        this.updateLevels();
     }
 
+    //get first element from seen queue and update max/min levels
+    //if queue is empty, check the recognize queue
     getSeen() {
-        // this.seen.print();
-        // console.log(this.seen);
         if (this.seen.isEmpty()) {
-            // this.minLevel = 2;
-            this.updateLevels();
             return this.getRecognize();
             
         } else {
             return this.seen.dequeue();
         }
+
+        this.updateLevels();
     }
 
+    //get max level
     getMaxLevel() {
         return this.maxLevel;
     }
 
+    //get min level
     getMinLevel() {
         return this.minLevel;
     }
 
+    //print info of all queues
     printQueues() {
         console.log("Seen:");
         this.seen.print();
@@ -253,121 +284,3 @@ class ProbabilityQueue {
         this.mastered.print();
     }
 }
-
-// class FavNode {
-//     constructor(id) {
-//         this.id = id;
-
-//         this.prev = null;
-//         this.next = null;
-//     }
-
-//     setNext (node) {
-//         this.next = node;
-//     }
-
-//     setPrev (node) {
-//         this.prev = node;
-//     }
-// }
-
-
-
-
-// //Flashcard Editors Sturcutres
-// class ElementNode {
-//     constructor(id, question, answer) {
-//         this.id = id;
-//         this.question = question;
-//         this.answer = answer;
-
-//         this.next = null;
-//     }
-
-//     setNext (node) {
-//         this.next = node;
-//     }
-
-//     debugPrinting() {
-//         let printString = this.id + " | " + this.question.value + " : " + this.answer.value;
-//         console.log(printString);
-//     }
-// }
-
-// class ElementQueue {
-//     constructor () {
-//         this.front = null;
-//         this.tail = null;
-//         this.size = 0;
-//     }
-
-//     enqueue (id, question, answer) {
-//         let newNode = new ElementNode(id, question, answer);
-
-//         if (this.size == 0) {
-//             this.front = newNode;
-//         } else {
-//             let prevTail = this.tail;
-//             prevTail.setNext(newNode);
-//         }
-
-//         this.tail = newNode;
-//         this.size++;
-//     }
-
-//     debugPrinting() {
-//         let current = this.front;
-
-//         while (current != null) {
-//             current.debugPrinting();
-//             current = current.next;
-//         }
-//     }
-// }
-
-// //Finished Flashcard Datastructures
-// class FinishedFlashcard {
-//     constructor (id, question, answer, q_img, a_img, star) {
-//         this.id = id;
-//         this.question = question;
-//         this.answer = answer;
-//         this.q_img = q_img;
-//         this.a_img = a_img;
-//         this.star = star;
-
-//         this.prev = null;
-//         this.next = null;
-//     }
-
-//     setPrev (node) {
-//         this.prev = node;
-//     }
-
-//     setNext (node) {
-//         this.next = node;
-//     }
-// }
-
-// class FlashcardQueue {
-//     constructor () {
-//         this.front = null;
-//         this.back = null;
-//         this.size = 0;
-//     }
-
-//     enqueue (id, question, answer, q_img, a_img, star) {
-//         let newNode = new FinishedFlashcard(id, question, answer, q_img, a_img, star);
-//         if (this.size == 0) {
-//             this.front = newNode;
-//             this.back = newNode;
-//         } else {
-//             let backNode = this.back;
-//             backNode.setNext(newNode);
-//             newNode.setPrev(backNode);
-//             this.back = newNode;
-//         }
-
-//         this.size++;
-
-//     }
-// }
