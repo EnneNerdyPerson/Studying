989a3d8870d5792f61a5de56026a5308776e8890
Enne <cheyenne03hug@gmail.com>
2026-07-21 20:59:27 -0400

    add comments/documentation and remove unneeded code from make page's js

--- js-scripts/make.js
+++ js-scripts/make.js
@@ -66,78 +66,106 @@ if (setname != "home-page-set-name") {
 //Set Name Script
 ///Set Name Information
 const nameContainer = document.getElementById("name-container");
-const setName = document.getElementById("set-name");
-
-let firstName = setName.innerHTML;
-
-const changeName = document.getElementById("change-set-name");
+const setNameElement = document.getElementById("set-name");
 
 ///Change Set Name Information
+const changeName = document.getElementById("change-set-name");
 const nameChangeContainer = document.getElementById("name-change-container");
 const changeNameInput = document.getElementById("set-name-change");
 const finChangeName = document.getElementById("save-set-name");
 
-let oldName = changeNameInput.value;
-
-//Form Stuff
+//Form 
 const form = document.getElementById("flashcard-maker");
 
-//Flashcard editing script
+//Buttons
+const buttonContainer = document.getElementById("button-container");
 const newCardButton = document.getElementById("new-card");
 const finishButton = document.getElementById("finish");
 
-const buttonContainer = document.getElementById("button-container");
+//global variables
+let oldName = changeNameInput.value;
 
-const firstQ = document.getElementById("q-text-1");
-const firstA = document.getElementById("a-text-1");
+let deleteStack = [];   //stack of deleted nums (to be reused when new card is made)
 
-let numQuestions = 0;
-if (makeNew) {
-    numQuestions = 1;
-} else {
-    numQuestions = questions.length;
+let numCards = 1;   //number of cards
+if (!makeNew) {
+    //if editing, the update number of cards
+    numCards = questions.length;
 }
-
-let deleteStack = [];
 
 //------------------------------------------------------------------------------
 //Functions ---------------------------
 //------------------------------------------------------------------------------
+/**
+ * Delete HTML elements for associated card
+ * 
+ * @param {*} numOfCard - HTML id of card to be deleted 
+ */
+function deleteCard(numOfCard) {
+    //save HTML id to be used for new card
+    deleteStack.push(numOfCard);
+
+    //decrease number of cards
+    numCards--;
+    
+    //remove HTML elements associated with card to be removed
+    const div = document.getElementById(numOfCard);
+    div.remove();
+}
+
+/**
+ * Create HTML elements for new card with HTML id = num and id = card_id
+ * (only for editing existing cards)
+ * 
+ * @param {*} num - number of cards
+ * @param {*} id - id for card (relevent only when editing)
+ * @param {*} questionValue - value of questionInput for card
+ * @param {*} answerValue - value of answerInput for card
+ */
 function newCard(num, id, questionValue, answerValue) {
+    //create new card container
     let newCardCreation = document.createElement("div");
     newCardCreation.classList.add("flex-container");
-    newCardCreation.id = num;
+    newCardCreation.id = num;       //used for card deletion
 
-    // Question Stuff Creation
+
+    // Question Creation
     let qContainerDiv = document.createElement("div");
     qContainerDiv.classList.add("card-item");
 
-    let questionText = document.createElement("label");
-    questionText.innerText = "Question";
-    questionText.htmlFor = "question[]";
-    qContainerDiv.append(questionText);
+    //question label
+    let questionLabel = document.createElement("label");
+    questionLabel.innerText = "Question";
+    questionLabel.htmlFor = "question[]";
+    qContainerDiv.append(questionLabel);
 
+    //question input
     let questionInput = document.createElement("input");
     questionInput.type = "text";
-    questionInput.id = id;
-    questionInput.name = "question[]";
+    questionInput.name = "question[]";      //question form data array
+    questionInput.id = id;                  //card_id for question
     questionInput.value = questionValue;
     qContainerDiv.append(questionInput);
 
-    // Answer Stuff Creation
+
+    // Answer Creation
     let aContainerDiv = document.createElement("div");
     aContainerDiv.classList.add("card-item");
 
+    //answer label
     let answerText = document.createElement("label");
     answerText.innerText = "Answer";
     answerText.htmlFor = "answer[]";
     aContainerDiv.append(answerText);
 
+    //answer input
     let answerInput = document.createElement("input");
     answerInput.type = "text";
-    answerInput.name = "answer[]";
+    answerInput.name = "answer[]";      //answer form data array
+    answerInput.id = id;                //card_id for question
     answerInput.value = answerValue;
     aContainerDiv.append(answerInput);
+
 
     //Delete Button Creation
     let buttonDiv = document.createElement("div");
@@ -145,36 +173,40 @@ function newCard(num, id, questionValue, answerValue) {
     buttonDiv.classList.add("button-container");
     buttonDiv.classList.add("flex-container");
 
+    //create delete button
     let deleteButton = document.createElement("input");
-    deleteButton.type = "button";
+    //add classes to delete button
     deleteButton.classList.add("button");
     deleteButton.classList.add("delete-button");
-    deleteButton.name = num;
+    //add type, value, name to delete button
+    deleteButton.type = "button";
     deleteButton.value = "Delete"
-    deleteButton.addEventListener('click', () => deleteCard(deleteButton));;
+    // deleteButton.name = num;
+    //add onclick function
+    deleteButton.addEventListener('click', 
+        () => deleteCard(num));
+    
     buttonDiv.append(deleteButton);
+
 
     //Add everything to newCardCreation
     newCardCreation.append(qContainerDiv);
     newCardCreation.append(aContainerDiv);
     newCardCreation.append(buttonDiv);
 
+    //add all values to body
     buttonContainer.before(newCardCreation);
 }
 
-function deleteCard(buttonElement) {
-    let numOfCard = buttonElement.name;
-    deleteStack.push(numOfCard);
-
-    numQuestions--;
-    
-    const div = document.getElementById(numOfCard);
-
-    div.replaceChildren();
-    div.remove();
-}
-
+/**
+ * makeSet
+ * When creating a new set, check for empty inputs. If nothign is empty then
+ * create new set in SQL database and add all cards to SQL database
+ * 
+ * @param {*} formData - form data with card infromation
+ */
 async function makeSet(formData) {
+    //get array of question and answer inputs from formData
     let questionInput = formData.getAll("question[]");
     let answerInput = formData.getAll("answer[]");
 
@@ -183,43 +215,33 @@ async function makeSet(formData) {
         return;
     }
 
+    //if input is empty string, do not save set
     for (let i of questionInput) {
         if (i == "") {
             return;
         }
     }
-
     for (let i of answerInput) {
         if (i == "") {
             return;
         }
     }
 
+    //get set_id for a new set
     let newSetid = (parseInt(numsets, 10) + 1).toString();
+    let setname = setNameElement.innerText;
     formData.append("numsets", newSetid);
-    
-    let checkSetResult;
 
     try {
-        let response = await fetch('http://localhost:3000/api/checkSet?set='+setName.innerText+'&userid='+userid);
+        //add new set for user with userid, setid, and setname
+        await fetch('http://localhost:3000/api/addSet?userid='+userid
+            +'&setid='+newSetid
+            +'&set='+setname
+        );
 
-        let data = await response.json();
-        let checkSetResult = data.data;
-
-        if (checkSetResult.length == 0) {
-            // console.log("No SET FOUND");
-            await fetch('http://localhost:3000/api/addSet?userid='+userid
-                    +'&setid='+newSetid
-                    // +'&setid='+'3'
-                    +'&set='+setName.innerText
-                );
-        } else {
-            //TODO: either remove or update so it has useful functionality
-            // console.log("SET EXISTS");
-            newSetid=checkSetResult[0]["set_id"];
-        }
-
+        //for every question/answer input, create/add card
         for (let i = 0; i < questionInput.length; i++) {
+            //add card for each question/answer input information
             await fetch('http://localhost:3000/api/addCard?userid='+userid
                     +'&setid='+newSetid
                     +'&cardid='+i
@@ -228,6 +250,7 @@ async function makeSet(formData) {
             );
         }
 
+        //run make-process php file for updating session info
         await fetch('scripts/make-process.php', {
             method: 'POST',
             body: formData
@@ -238,43 +261,64 @@ async function makeSet(formData) {
     }
 }
 
+/**
+ * editSet
+ * 
+ * Validate some infromation to edit set infromation. If validation is met, update
+ * information in SQL database - delete deleted card, update question or answer 
+ * infromation, and add newly created cards
+ *
+ * @param {*} formData - form data with card infromation
+ * @returns 
+ */
 async function editSet(formData) {
-    let questionIds = Array.from(document.getElementsByName('question[]')).map(question => question.id);
+    //card_id for each HTML card element
+    let cardElementIds = Array.from(document.getElementsByName('question[]')).map(question => question.id);
 
-
+    //question and answer input information
     let questionInput = formData.getAll("question[]");
     let answerInput = formData.getAll("answer[]");
 
-    if (answerInput.length != questionInput.length) {
-        return;
+    //if input is empty string, do not save set
+    for (let i of questionInput) {
+        if (i == "") {
+            return;
+        }
+    }
+    for (let i of answerInput) {
+        if (i == "") {
+            return;
+        }
     }
 
-    let alteredIds = [];
-    let questionClean = [];
-    let index = 0;
-    let minCheck = 0;
-    let maxId = 0;
+    let alteredIds = [];        //card_ids of cards that were changed
+    let missingIdStack = [];    //card_ids of cards that were deleted (for reuse)
 
-    let missingIdStack = [];
+    let index = 0;      //index of cardElementIds and index of first new card (no card_id)
 
+    //iterate through cardIds to check if there is change
     for (let i = 0; i < cardId.length; i++) {
-        if (questionIds[index] == "-1") {
+        //if cardElementId is "-1", then this is new card
+        if (cardElementIds[index] == "-1") {
             break;
-        } else if (cardId[i] == questionIds[index]) {
-            console.log(questionIds[index]);
-            console.log(questionInput[index]);
-            console.log(questions[cardId[i]]);
 
-            if (questionInput[index] != questions[cardId[i]] ||
-                answerInput[index] != answers[cardId[i]]
-            ) {
+        //check if card with card_id has been edited
+        } else if (cardId[i] == cardElementIds[index]) {
+            //check for question or answer change
+            if (questionInput[index] != questions[cardId[i]] || answerInput[index] != answers[cardId[i]] ) {
+                //if changed, add to aleterdIds
                 alteredIds.push(index);
             }
             
+            //increase index
             index++;
+
+        //if card_id is not found
         } else {
+            //add card_id to missingIdStack
             missingIdStack.push(cardId[i]);
 
+            //delete card from SQL database
             try {
                 await fetch('http://localhost:3000/api/deleteCard?userid='+userid
                         +'&setid='+setid
@@ -287,19 +331,16 @@ async function editSet(formData) {
         }
     }
 
+    //for cards with card_id that were altered, update in SQL database
     for (let i = 0; i < alteredIds.length; i++) {
+        //save current id
         let curIndex = alteredIds[i];
 
-        if (questionInput[curIndex] == "") {
-            return;
-        } else if (answerInput[curIndex] == "") {
-            return;
-        }
-
+        //update question and answer for card in SQL database
         try {
             await fetch('http://localhost:3000/api/updateCardQA?userid='+userid
                     +'&setid='+setid
-                    +'&cardid='+questionIds[curIndex]
+                    +'&cardid='+cardElementIds[curIndex]
                     +'&question='+questionInput[i]
                     +'&answer='+answerInput[i]
             );
@@ -309,23 +350,21 @@ async function editSet(formData) {
         }
     }
 
+    let curCardId = index;     //for reusing card_ids (so numbers aren't too high)
 
+    //for all cards without card_id (new cards)
     for (let i = index; i < questionInput.length; i++) {
-
-        if (questionInput[i] == "") {
-            return;
-        } else if (answerInput[i] == "") {
-            return;
-        }
-
-        let cardId = 0;
+        let cardId = curCardId;
 
         if (missingIdStack.length != 0) {
+            //reuse deleted card's card_id
             cardId = missingIdStack.pop();
         } else {
-            cardId = i;
+            //else get new card_id
+            curCardId++;
         }
 
+        //create new card with given information
         try {
             await fetch('http://localhost:3000/api/addCard?userid='+userid
                     +'&setid='+setid
@@ -339,26 +378,33 @@ async function editSet(formData) {
         }
     }
 }
-//------------------------------------------------------------------------------
-//Event Listeners ---------------------------
-//------------------------------------------------------------------------------
-///Show Change Name HTML
-changeName.addEventListener("click", function() {
-    changeNameInput.value = setName.innerHTML;
 
+//------------------------------------------------------------------------------
+//Event Listeners --------------------------------------------------------------
+//------------------------------------------------------------------------------
+
+//Show Change Name HTML when changeName butt is clicked
+changeName.addEventListener("click", function() {
+    //update changeNameInput to current setname
+    changeNameInput.value = setNameElement.innerHTML;
+
+    //hide name and show nameChange information
     nameContainer.classList.toggle("hidden");
     nameChangeContainer.classList.toggle("hidden");
 });
 
-///Save name changes
+//Save name changes when finChangeName button is clicked
 finChangeName.addEventListener("click", function () {
-    console.log("change button made");
+    //update setName element to new name
+    setNameElement.innerHTML = changeNameInput.value;
 
-    setName.innerHTML = changeNameInput.value;
-
+    //if there was a name change and not making new set
     if ((!makeNew) && (oldName != changeNameInput.value)) {
+        //update setname in database
         try {
-            fetch('http://localhost:3000/api/updateSetName?setname='+setName.innerHTML+'&userid='+userid+'&setid='+setid);
+            fetch('http://localhost:3000/api/updateSetName?setname='+setNameElement.innerHTML
+                + '&userid=' + userid
+                + '&setid=' + setid);
 
         } catch (error) {
             //print to console any errors that occur
@@ -366,55 +412,74 @@ finChangeName.addEventListener("click", function () {
         }
     }
 
+    //set setname and hide name change elements
     nameContainer.classList.toggle("hidden");
     nameChangeContainer.classList.toggle("hidden");
 });
 
+//when newCard button is pressed, add new card HTML elements
 newCardButton.addEventListener("click", function () {
-    numQuestions++;
-    let num = 0;
+    numCards++;     //increase number of cards count
+    let num = 0;    //num for HTML id value
 
     if (deleteStack.length != 0) {
+        //reuse previously deleted HTML ids
         num = deleteStack.pop();    
     } else {
-        num = numQuestions;
+        //use numCards has HTML id
+        num = numCards;
     }
 
+    //create new card HTML elements with newCard function
     newCard(num, "-1", "", "");
 });
 
+//when form is submited, either make or edit set, then go to study page
 form.addEventListener("submit", async function() {
     // Stop the form from submitting normally and reloading
     event.preventDefault(); 
+
+    //for debugging
     console.log("finish button click!");
 
+    //save form data to be used for checks and php files
     const formData = new FormData(this);    //formatting from data
+
+    //check if making new set or editing current set
     if (makeNew) {
+        //wait for makeSet function to finish
         await makeSet(formData);
     } else {
+        //wait for editSet function to finish
         await editSet(formData);
     }
     
-    console.log("finish button click 2!");
+    //move to study page
     window.location.href = "flashcard-study.php";
 });
 
 //------------------------------------------------------------------------------
-//Start function ---------------------------
+//Start function ---------------------------------------------------------------
 //------------------------------------------------------------------------------
+//Start function to start the page
 function start(makeNew) {
+    //check if creating new set or editing current set
     if (makeNew) {
+        //create sigle card with no card_id
         newCard("1", "-1", "", "");
     } else {
+        //update setname based on set being edited
         changeNameInput.value = setname;
-        setName.innerHTML = changeNameInput.value;
+        setNameElement.innerHTML = changeNameInput.value;
 
+        //for ever card in set, create new card HTML elements
         for (let i = 0; i < questions.length; i++) {
-            let num = i + 1;
-
-            newCard(num, cardId[i], questions[i], answers[i]);
+            //HTML id = i + 1, card_id = cardId[i]
+            //populates question and answer input with card's question and answer info
+            newCard(i + 1, cardId[i], questions[i], answers[i]);
         }
     }
 }
 
+//start page
 start(makeNew);
