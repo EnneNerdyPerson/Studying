536532320d5d2d23e02030f1f1b3769ba6442b14
Enne <cheyenne03hug@gmail.com>
2026-07-28 15:49:18 -0400

    organize make html more

--- flashcard-make.php
+++ flashcard-make.php
@@ -16,7 +16,6 @@
     <div class="header flex-container">
         <!--Logo and Website Name-->
         <div class="flex-container">
-            <!-- <h1 id="logo" >Logo</h1> -->
             <img id="logo" src="images/Logo.png" alt="Logo">
             <h2 id="website-name">Flash Study</h2>
         </div>
@@ -52,7 +51,7 @@
     <!-- Set Name Elements -->
     <div id="name-container" class="flex-container">
         <!--Displaying the set name-->
-        <label>Set Name: </label>
+        <!-- <label>Set Name: </label> -->
         <h2 id="set-name">Untitled</h2>
 
         <!--Button for changing the set name-->
@@ -65,9 +64,9 @@
     <!-- Flash Cards Elements (form) -->
     <form method="post" class="" id="flashcard-maker">
         <!--Set Name Change Elements-->
-        <div class="flex-container hidden" id="name-change-container">
+        <div id="name-change-container" class="flex-container hidden">
             <!--New Set Name Input-->
-            <label for="set-name-change">Set Name: </label>
+            <!-- <label for="set-name-change">Set Name: </label> -->
             <input id="set-name-change" 
                 type="text" 
                 name="set-name-change" 
